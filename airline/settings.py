154c40fed22913053e1fb17b983a14857d5c7c7c
mojib2014 <mojib_monir@yahoo.com>
2022-01-07 16:26:40 -0800

    Add Docker (CD), Create 2 containers 1 for web 1 for db

--- airline/settings.py
+++ airline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gp(3z@@pc+4=x!*pbdtfro=n$9n=xb(uu_7&&jh_-)p87x#o-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
